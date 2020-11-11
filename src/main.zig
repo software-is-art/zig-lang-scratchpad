e959d1933eeb42dd0dc9ae27c989e9838e8ca796
software-is-art <callum.galbreath@live.com>
2020-11-11 04:08:17 +0000

    Working through section 1 of the documentation (skipped ahead to generics briefly)

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,128 @@
 const std = @import("std");
+const expect = @import("std").testing.expect;
+
+test "if statement" {
+    const a = true;
+    var x: u16 = 0;
+    if (a) {
+        x += 1;
+    } else {
+        x += 2;
+    }
+    expect(x == 1);
+}
+
+test "if statement expression" {
+    const a = true;
+    var x: u16 = 0;
+    x += if (a) 1 else 2;
+    expect(x == 1);
+}
+
+test "while" {
+    var i: u8 = 2;
+    while (i < 100) {
+        i *= 2;
+    }
+    expect(i == 128);
+}
+
+test "while with a continue expression" {
+    var sum: u8 = 0;
+    var i: u8 = 1;
+    while (i <= 10) : (i += 1) {
+        sum += i;
+    }
+    expect(sum == 55);
+}
+
+test "while with continue" {
+    var sum: u8 = 0;
+    var i: u8 = 0;
+    while (i <= 3) : (i += 1) {
+        if (i == 2) continue;
+        sum += i;
+    }
+    expect(sum == 4);
+}
+
+test "while with break" {
+    var sum: u8 = 0;
+    var i: u8 = 0;
+    while (i <= 3) : (i += 1) {
+        if (i == 2) break;
+        sum += i;
+    }
+    expect(sum == 1);
+}
+
+test "for" {
+    const string = [_]u8{ 'a', 'b', 'c' };
+
+    for (string) |character, index| {}
+
+    for (string) |character| {}
+
+    for (string) |_, index| {}
+
+    for (string) |_| {} 
+}
+
+test "function" {
+    const y = addFive(0);
+    expect(@TypeOf(y) == u32);
+    expect(y == 5);
+}
+
+fn addFive(x: u32) u32 {
+    return x + 5;
+}
+
+test "function recursion" {
+    const x = fibonacci(10);
+    expect(x == 55);
+}
+
+fn fibonacci(n: u16) u16 {
+    if (n == 0 or n == 1) return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+test "defer" {
+    var x: i16 = 5;
+    {
+        defer x += 2;
+        expect(x == 5);
+    }
+    expect(x == 7);
+}
+
+test "multi defer" {
+    var x: f32 = 5;
+    {
+        defer x += 2;
+        defer x /= 2;
+    }
+    expect(x == 4.5);
+}
+
+test "Generics" {
+    const len: usize = 3;
+    var array = [_]i32{1, 2, 3};
+    var list = List(i32) {
+        .items = &array,
+        .len = len,
+    };
+}
+
+fn List(comptime T: type) type {
+    return struct {
+        items: []T,
+        len: usize,
+    };
+}
 
 pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
+    const a = [_]u8{ 1, 2, 3 };
+    std.log.info("Hello, {}!\n", .{"World"});
 }
