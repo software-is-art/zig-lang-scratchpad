a3a3c30c7a598061d06509a08b5c2ba7a3d3f829
software-is-art <callum.galbreath@live.com>
2020-11-12 21:00:41 +0000

    Completed error sets. Up to switch expressions.

--- src/main.zig
+++ src/main.zig
@@ -128,7 +128,7 @@ const FileOpenError = error {
     FileNotFound
 };
 
-const AllocationError = error {OutOfMemory};
+const AllocationError = error{OutOfMemory};
 
 test "coerce error from a subset to a superset" {
     const err: FileOpenError = AllocationError.OutOfMemory;
@@ -142,6 +142,57 @@ test "error union" {
     expect(@TypeOf(no_error) == u16);
     expect(no_error == 10);
 }
+
+fn failingFunction() error{Oops}!void {
+    return error.Oops;
+}
+
+test "returning an error" {
+    failingFunction() catch |err| {
+        expect(err == error.Oops);
+        return;
+    };
+}
+
+fn failingFn() error{Oops}!i32 {
+    try failingFunction();
+    return 12;
+}
+
+test "try" {
+    var v = failingFn() catch |err| {
+        expect(err == error.Oops);
+        return;
+    };
+    expect(v == 12); // Unreachable in this example
+}
+
+var problems: u32 = 98;
+
+fn failFnCounter() error{Oops}!void {
+    errdefer problems += 1;
+    try failingFunction();
+}
+
+test "errdefer" {
+    failFnCounter() catch |err| {
+        expect(err == error.Oops);
+        expect(problems == 99);
+        return;
+    };
+}
+
+fn createFile() !void {
+    return error.AccessDenied;
+}
+
+test "inferred error set" {
+    const x: error{AccessDenied}!void = createFile();
+}
+
+const A = error{ NotDir, PathNotFound };
+const B = error{ OutOfMemory, PathNotFound };
+const C = A || B;
 
 pub fn main() anyerror!void {
     const a = [_]u8{ 1, 2, 3 };
