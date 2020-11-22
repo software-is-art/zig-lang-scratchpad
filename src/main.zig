a3e6f881cf9aa2772ca5d2057b08b003a76b386d
software-is-art <callum.galbreath@live.com>
2020-11-22 20:50:21 +0000

    Complete up to Structs

--- src/main.zig
+++ src/main.zig
@@ -194,6 +194,163 @@ const A = error{ NotDir, PathNotFound };
 const B = error{ OutOfMemory, PathNotFound };
 const C = A || B;
 
+test "switch statement" {
+    var x: i8 = 10;
+    switch (x) {
+        -1...1 => {
+            x = -x;
+        },
+        10, 100 => {
+            x = @divExact(x, 10);
+        },
+        else => {},
+    }
+    expect(x == 1);
+}
+
+test "switch expression" {
+    var x: i8 = 10;
+    x = switch (x) {
+        -1...1 => -x,
+        10, 100 => @divExact(x, 10),
+        else => x,
+    };
+    expect(x == 1);
+}
+
+test "out of bounds" {
+    const a = [3]u8{ 1, 2, 3 };
+    var index: u8 = 5;
+    //const b = a[index];
+}
+
+test "unreachable" {
+    const x: i32 = 1;
+    //const y: u32 = if (x == 2) 5 else unreachable;
+}
+
+fn asciiToUpper(x: u8) u8 {
+    return switch (x) {
+        'a'...'z' => x + 'A' - 'a',
+        'A'...'Z' => x,
+        else => unreachable,
+    };
+}
+
+test "unreachable switch" {
+    expect(asciiToUpper('a') == 'A');
+    expect(asciiToUpper('A') == 'A');
+}
+
+fn increment(num: *u8) void {
+    num.* += 1;
+}
+
+test "pointers" {
+    var x: u8 = 1;
+    increment(&x);
+    expect(x == 2);
+}
+
+test "naughty pointer" {
+    var x: u16 = 0;
+    //var y: *u8 = @intToPtr(*u8, x);
+}
+
+test "const pointers" {
+    const x: u8 = 1;
+    var y = &x;
+    //y.* += 1;
+}
+
+test "usize" {
+    expect(@sizeOf(usize) == @sizeOf(*u8));
+    expect(@sizeOf(isize) == @sizeOf(*u8));
+}
+
+fn total(values: []const u8) usize {
+    var count: usize = 0;
+    for (values) |v| count += v;
+    return count;
+}
+
+test "slices" {
+    const array = [_]u8{ 1, 2, 3, 4, 5 };
+    const slice = array[0..3];
+    expect(total(slice) == 6);
+}
+
+test "slices 2" {
+    const array = [_]u8{ 1, 2, 3, 4, 5 };
+    const slice = array[0..3];
+    expect(@TypeOf(slice) == *const [3]u8);
+}
+
+test "slices 3" {
+    var array = [_]u8{ 1, 2, 3, 4, 5 };
+    var slice = array[0..];
+}
+
+const Direction = enum { north, south, east, west };
+const Value = enum(u2) { zero, one, two };
+
+test "enum ordinal value" {
+    expect(@enumToInt(Value.zero) == 0);
+    expect(@enumToInt(Value.one) == 1);
+    expect(@enumToInt(Value.two) == 2);
+}
+
+const Value2 = enum(u32) {
+    hundred = 100,
+    thousand = 1000,
+    million = 1000000,
+    next,
+};
+
+test "set enum ordinal value" {
+    expect(@enumToInt(Value2.hundred) == 100);
+    expect(@enumToInt(Value2.thousand) == 1000);
+    expect(@enumToInt(Value2.million) == 1000000);
+    expect(@enumToInt(Value2.next) == 1000001);
+}
+
+const Suit = enum {
+    clubs,
+    spades,
+    diamonds,
+    hearts,
+    pub fn isClubs(self: Suit) bool {
+        return self == Suit.clubs;
+    }
+};
+
+test "enum method" {
+    expect(Suit.spades.isClubs() == Suit.isClubs(.spades));
+}
+
+const Mode = enum {
+    var count: u32 = 0;
+    on,
+    off,
+};
+
+test "hmm" {
+    Mode.count += 1;
+    expect(Mode.count == 1);
+}
+
+const Vec3 = struct {
+    x: f32, y: f32, z: f32
+};
+
+test "struct usage" {
+    const my_vector = Vec3{
+        .x = 0,
+        .y = 100,
+        .z = 50,
+    };
+}
+
 pub fn main() anyerror!void {
     const a = [_]u8{ 1, 2, 3 };
     std.log.info("Hello, {}!\n", .{"World"});
